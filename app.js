d0e7b5f1d602586360014de9bc71a564618b299d
deepak kumar <deepak.kumar@nourishgenie.in>
2023-08-18 23:55:12 +0530

    adding route to handle different endpoint

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log("deepak kamboj");
+    const url = req.url;
+    if (url === '/home') {
+        res.write('<h1>Wecome Home</h1>')
+        return res.end()
+    }
+    if (url === '/about') {
+        res.write('<h1>Welcome to About Us page</h1>')
+        return res.end()
+    }
+    if (url === '/node') {
+        res.write('<h1>Welcome to my Node Js project</h1>')
+        return res.end()
+    }
 })
 
 server.listen(3000);
