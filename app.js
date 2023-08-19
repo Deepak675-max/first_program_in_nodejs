8109d80acab74f04fc9efd70c98515588acf3897
deepak kumar <deepak.kumar@nourishgenie.in>
2023-08-19 14:31:37 +0530

    adding add-product and product routes to hanlde form input and parsed the input data

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("this is the first middleware!");
-    next();
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<body><form action="/product" method="POST"><input type="text" name="name"><input type="text" name="size"><button type="submit">Add Product</button></form></body>');
 })
 
-app.use((req, res, next) => {
-    console.log("this is the second middleware!");
-    next();
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1> Welcome to the express app</h1>');
 })
 
 app.listen(3000);
