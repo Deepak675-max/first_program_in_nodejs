7fa4fd767461d81e302189df14099e21c4ccc895
deepak kumar <deepak.kumar@nourishgenie.in>
2023-08-19 14:53:58 +0530

    added router in the app and also added 404 page

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<body><form action="/product" method="POST"><input type="text" name="name"><input type="text" name="size"><button type="submit">Add Product</button></form></body>');
-})
+app.use(adminRoutes);
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.send(req.body);
-})
+app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-    res.send('<h1> Welcome to the express app</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 
 app.listen(3000);
